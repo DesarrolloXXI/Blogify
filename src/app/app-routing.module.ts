5e57210072ddbb1d6efbb910642cc98177d1caa2
Alvaro Hernandez <alvarojhr96@gmail.com>
2021-10-28 08:03:29 -0500

    Adding create post component and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { CreatePostComponent } from './create-post/create-post.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'add', component: CreatePostComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
