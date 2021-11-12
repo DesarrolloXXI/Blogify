73dfd8d87314c7fe94c64e66eb125a5e8c424ca5
Alvaro Hernandez <alvarojhr96@gmail.com>
2021-11-12 12:27:51 -0500

    connect database

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
-var cors = require("cors");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
 const app = express();
 
 const postRoutes = require("./routes/posts");
@@ -11,6 +13,16 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
 // Configuracion de la conexion a la bd
+mongoose
+  .connect(
+    "mongodb+srv://dbUser:BWn65Euo8jT5QGik@cluster0.h306v.mongodb.net/Blogify?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("Estamos conectados a nuestra BD");
+  })
+  .catch(() => {
+    console.log("Houston tenemos un problema");
+  });
 
 app.use("/api/posts", postRoutes);
 
